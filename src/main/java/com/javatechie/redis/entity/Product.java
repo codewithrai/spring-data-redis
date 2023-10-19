d766cf096681a430fa611f7fbfc2ed08d7c0287c
Waqar Ahmad <wahmad@epteck.com>
2023-10-19 10:13:00 +0500

    database properties udpated

--- src/main/java/com/javatechie/redis/entity/Product.java
+++ src/main/java/com/javatechie/redis/entity/Product.java
@@ -4,18 +4,23 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import org.springframework.data.annotation.Id;
 import org.springframework.data.redis.core.RedisHash;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import java.io.Serializable;
 
 @Setter
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
+@Entity
 @RedisHash("Product")
 public class Product implements Serializable {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
     private String name;
     private int quantity;
